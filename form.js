3de4ddba1bf1131606baa8212f4b117364c6709f
kanainjapan <66121506+kanainjapan@users.noreply.github.com>
2020-07-15 22:24:13 +0900

    Added Images

--- form.js
+++ form.js
@@ -1,9 +1,11 @@
-document.querySelector('.ball').addEventListener('click',
-  function() {
-    document.querySelector('.popup').style.display = 'flex';
-});
+
 
 document.querySelector('.close-form').addEventListener('click',
   function() {
     document.querySelector('.form').style.display = 'none';
+});
+
+document.querySelector('.ball').addEventListener('click',
+  function() {
+    document.querySelector('.popup').style.display = 'flex';
 });
