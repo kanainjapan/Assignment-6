00cb548d3d7f558db4aa1e42b2c8502e97648297
kanainjapan <66121506+kanainjapan@users.noreply.github.com>
2020-07-16 20:06:34 +0900

    Finished

--- form.js
+++ form.js
@@ -1,11 +1,9 @@
-
+document.querySelector('.open-form').addEventListener('click',
+  function() {
+    document.querySelector('.form').style.display = 'flex';
+});
 
 document.querySelector('.close-form').addEventListener('click',
   function() {
     document.querySelector('.form').style.display = 'none';
-});
-
-document.querySelector('.ball').addEventListener('click',
-  function() {
-    document.querySelector('.popup').style.display = 'flex';
 });
